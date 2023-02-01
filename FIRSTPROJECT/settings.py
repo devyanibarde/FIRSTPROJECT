4050775d02e34d34aaea9397c887c38c27ce0a9b
Devyani Barde <devyanibarde@gmail.com>
2023-02-01 17:35:38 -0500

    static files settings

--- FIRSTPROJECT/settings.py
+++ FIRSTPROJECT/settings.py
@@ -16,6 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = BASE_DIR.joinpath('templates')
+STATIC_DIR = BASE_DIR.joinpath('static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -113,6 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
