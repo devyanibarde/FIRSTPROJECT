11ab16fe8fc506b4e9e79e6831b61949bedede16
Devyani Barde <devyanibarde@gmail.com>
2023-02-02 10:54:52 -0500

    created and registered models

--- FIRSTAPPLICATION/admin.py
+++ FIRSTAPPLICATION/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from FIRSTAPPLICATION.models import *
 
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(Contact)
+admin.site.register(Address)
--- FIRSTAPPLICATION/models.py
+++ FIRSTAPPLICATION/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    def __str__(self) -> str:
+        return self.first_name
+
+class Contact(models.Model):
+    pid = models.ForeignKey(Person, on_delete=models.CASCADE)
+    phone_no = models.CharField(max_length=100)
+    email_id = models.CharField(max_length=200)
+    def __str__(self) -> str:
+        return self.pid
+
+class Address(models.Model):
+    pid = models.ForeignKey(Person, on_delete=models.CASCADE)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    def __str__(self) -> str:
+        return self.pid
